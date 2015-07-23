12c32372cfc32412abce941a65d5c16735d4f86e
Alan Berndt <alan@eatabrick.org>
2015-07-22 18:33:44 -0700

    Implement in C

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "node.h"
+#include "maze.h"
+
+int main() {
+  maze maze;
+
+  maze_init(&maze);
+  maze_read(&maze, stdin);
+  maze_astar(&maze);
+  maze_print_path(&maze);
+}
