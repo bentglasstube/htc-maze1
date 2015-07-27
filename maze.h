7fe75aa40a73ab4af4a0ae941ce262bfe50bb0fe
Alan Berndt <alan@eatabrick.org>
2015-07-27 12:13:23 -0700

    Remove defunct function definition

--- maze.h
+++ maze.h
@@ -16,5 +16,4 @@ void maze_init(maze *maze);
 void maze_read(maze *maze, FILE *stream);
 void maze_astar(maze *maze);
 void maze_print_path(maze *maze);
-void maze_print_scores(maze *maze);
 void maze_free(maze *maze);
